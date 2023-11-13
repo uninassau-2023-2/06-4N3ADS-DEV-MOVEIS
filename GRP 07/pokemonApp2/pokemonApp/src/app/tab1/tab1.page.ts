0071d726954e5525a2047d4086a4dc2cdc1e06f1
myllenasgl <myllasgomesl@gmail.com>
2023-11-12 23:23:34 -0300

    PokemonApp2

---
+++ GRP 07/pokemonApp2/pokemonApp/src/app/tab1/tab1.page.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { PokeAPIService } from '../services/poke-api.service';
+import { ViaCEPService } from '../services/via-cep.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  areaBuscarPokemon: string = '52011210';
+  areaBusca: any = {
+    bairro: '',
+    localidade: '',
+    logradouro: '',
+    uf: ''
+  };
+
+  // Adicione uma variável para armazenar o número de habilidades do Pokémon
+  bulbasaurSkills: number = 5;
+
+  constructor(
+    private navCtrl: NavController,
+    private pokeAPIService: PokeAPIService,
+    private viaCepService: ViaCEPService
+  ) {}
+
+  buscarPokemon() {
+    this.viaCepService.getViaCEPService(this.areaBuscarPokemon)
+      .subscribe((value) => {
+        this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
+        this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value))['bairro'];
+        this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value))['localidade'];
+        this.areaBusca.uf = '-' + JSON.parse(JSON.stringify(value))['uf'];
+
+        console.log('Bulbasaur Skills antes de navegar para tab2:', this.bulbasaurSkills);
+        // Navegue para a tab2 passando o número de habilidades do Bulbasaur
+        this.navCtrl.navigateForward('/tabs/tab2', {
+          queryParams: {
+            bulbasaurSkills: this.bulbasaurSkills
+            
+          }
+          
+        });
+      });
+
+    this.pokeAPIService.getPokeAPIService();
+  }
+}
